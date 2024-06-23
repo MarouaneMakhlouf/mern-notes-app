013d82cbeb3545e94f92f309cba93989c106191d
marouane <marwanmakhloufraja@gmail.com>
2024-06-23 22:17:42 +0100

    add pages and routers for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
-import './index.css'
+import './index.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { Navbar } from './components/Navbar';
+import { Routes , Route } from 'react-router-dom';
+import PrivateRoutes from './utils/PrivateRoutes';
+import Notes from './pages/Notes';
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div >
+      <Navbar/>
+      <Routes>
+        <Route element={<PrivateRoutes/>} >
+          <Route path='/notes' element={<Notes/>} />
+        </Route>
+        <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </div>
   )
 }
 
